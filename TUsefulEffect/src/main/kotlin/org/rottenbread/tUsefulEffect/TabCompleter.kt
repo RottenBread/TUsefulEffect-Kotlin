096e2823c1870088c12e4b4392468f69381df133
Junhyeok Moon <mig070@naver.com>
2025-01-19 16:54:05 +0900

    Update TabCompleter.kt

--- TUsefulEffect/src/main/kotlin/org/rottenbread/tUsefulEffect/TabCompleter.kt
+++ TUsefulEffect/src/main/kotlin/org/rottenbread/tUsefulEffect/TabCompleter.kt
@@ -1,4 +1,14 @@
 package org.rottenbread.tUsefulEffect
 
-class TabCompleter {
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
+
+class TabCompleter : TabCompleter {
+    override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<out String>): List<String>? {
+        if (args.size == 1) {
+            return listOf("invisibility", "night_vision").filter { it.startsWith(args[0], true) }
+        }
+        return null
+    }
 }
